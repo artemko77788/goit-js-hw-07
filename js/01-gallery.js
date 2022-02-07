b5ebeb119b90cb347ea886a81b8b9b595d6084fd
Artem Mironov <31404136+artemko77788@users.noreply.github.com>
2022-02-07 15:15:04 +0200

    first-home-work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const pictureMarkap = createGallery(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", pictureMarkap);
+gallery.addEventListener("click", onPictureClick);
+
+function createGallery(picture) {
+  return picture
+    .map(({ preview, original, description }) => {
+      return ` <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
+}
+
+function onPictureClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src= "${event.target.dataset.source}" >
+`);
+
+  instance.show();
+}
